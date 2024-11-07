7212154f51a1575930a9b3c9640733c76bbdee60
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2024-11-07 00:37:43 +0000

    Backtracking

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,73 +1,44 @@
 #!/usr/bin/python3
-""" Module definition for n-queens problem """
+""" N queens """
 import sys
 
 
-def backtrack(n, r, col, pos_diag, neg_diag, board, result, valid):
-    """ recursive function to check different placements """
-    if r == n:
-        copy = [row.copy() for row in board]
-        result.append(copy)
-        return
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    for c in range(n):
-        if c in col or (r + c) in pos_diag or (r - c) in neg_diag:
-            continue
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-        col.add(c)
-        pos_diag.add(r + c)
-        neg_diag.add(r - c)
-        board[r][c] = [r, c]
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-        valid.append(board[r][c])
+n = int(sys.argv[1])
 
-        backtrack(n, r + 1, col, pos_diag, neg_diag, board, result, valid)
 
-        col.remove(c)
-        pos_diag.remove(r + c)
-        neg_diag.remove(r - c)
-        board[r][c] = 0
-    return result
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
 
 def solve(n):
-    """ initializes trackers and call recursive function """
-    col = set()
-    pos_diag = set()
-    neg_diag = set()
-
-    result = []
-    valid = []
-    board = [[0] * n for i in range(n)]
-
-    backtrack(n, 0, col, pos_diag, neg_diag, board, result, valid)
-
-    return result
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    results = solve(n)
-
-    for res in results:
-        valid_results = []
-        for row in res:
-            valid_row = []
-            for cell in row:
-                if cell != 0:
-                    valid_row.extend(cell)
-            valid_results.append(valid_row)
-        print(valid_results)
+solve(n)
