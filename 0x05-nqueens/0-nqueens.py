76955441861ba6b5f528d82512f7bc4f0af7b3a4
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2024-11-07 00:35:50 +0000

    Backtracking

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,113 +1,73 @@
 #!/usr/bin/python3
-"""N queens solution finder module.
-"""
+""" Module definition for n-queens problem """
 import sys
 
 
-solutions = []
-"""The list of possible solutions to the N queens problem.
-"""
-n = 0
-"""The size of the chessboard.
-"""
-pos = None
-"""The list of possible positions on the chessboard.
-"""
+def backtrack(n, r, col, pos_diag, neg_diag, board, result, valid):
+    """ recursive function to check different placements """
+    if r == n:
+        copy = [row.copy() for row in board]
+        result.append(copy)
+        return
+
+    for c in range(n):
+        if c in col or (r + c) in pos_diag or (r - c) in neg_diag:
+            continue
+
+        col.add(c)
+        pos_diag.add(r + c)
+        neg_diag.add(r - c)
+        board[r][c] = [r, c]
+
+        valid.append(board[r][c])
+
+        backtrack(n, r + 1, col, pos_diag, neg_diag, board, result, valid)
+
+        col.remove(c)
+        pos_diag.remove(r + c)
+        neg_diag.remove(r - c)
+        board[r][c] = 0
+    return result
 
 
-def get_input():
-    """Retrieves and validates this program's argument.
+def solve(n):
+    """ initializes trackers and call recursive function """
+    col = set()
+    pos_diag = set()
+    neg_diag = set()
 
-    Returns:
-        int: The size of the chessboard.
-    """
-    global n
-    n = 0
+    result = []
+    valid = []
+    board = [[0] * n for i in range(n)]
+
+    backtrack(n, 0, col, pos_diag, neg_diag, board, result, valid)
+
+    return result
+
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+
     try:
         n = int(sys.argv[1])
-    except Exception:
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
+
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    return n
 
+    results = solve(n)
 
-def is_attacking(pos0, pos1):
-    """Checks if the positions of two queens are in an attacking mode.
-
-    Args:
-        pos0 (list or tuple): The first queen's position.
-        pos1 (list or tuple): The second queen's position.
-
-    Returns:
-        bool: True if the queens are in an attacking position else False.
-    """
-    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
-        return True
-    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
-
-
-def group_exists(group):
-    """Checks if a group exists in the list of solutions.
-
-    Args:
-        group (list of integers): A group of possible positions.
-
-    Returns:
-        bool: True if it exists, otherwise False.
-    """
-    global solutions
-    for stn in solutions:
-        i = 0
-        for stn_pos in stn:
-            for grp_pos in group:
-                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
-                    i += 1
-        if i == n:
-            return True
-    return False
-
-
-def build_solution(row, group):
-    """Builds a solution for the n queens problem.
-
-    Args:
-        row (int): The current row in the chessboard.
-        group (list of lists of integers): The group of valid positions.
-    """
-    global solutions
-    global n
-    if row == n:
-        tmp0 = group.copy()
-        if not group_exists(tmp0):
-            solutions.append(tmp0)
-    else:
-        for col in range(n):
-            a = (row * n) + col
-            matches = zip(list([pos[a]]) * len(group), group)
-            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
-            group.append(pos[a].copy())
-            if not any(used_positions):
-                build_solution(row + 1, group)
-            group.pop(len(group) - 1)
-
-
-def get_solutions():
-    """Gets the solutions for the given chessboard size.
-    """
-    global pos, n
-    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
-    a = 0
-    group = []
-    build_solution(a, group)
-
-
-n = get_input()
-get_solutions()
-for solution in solutions:
-    print(solution)
+    for res in results:
+        valid_results = []
+        for row in res:
+            valid_row = []
+            for cell in row:
+                if cell != 0:
+                    valid_row.extend(cell)
+            valid_results.append(valid_row)
+        print(valid_results)
